822bdc5011d3a8add344671682fa3efe9c59ff35
Kyle Terry <kyle@kyleterry.com>
2020-09-14 09:42:54 -0700

    adds spacetime board

---
+++ keyboards/spacetime/rev2/default/rules.mk
@@ -0,0 +1,6 @@
+OLED_DRIVER_ENABLE            = no
+RGBLIGHT_ENABLE               = yes
+LINK_TIME_OPTIMIZATION_ENABLE = no
+LEADER_ENABLE                 = no
+SPACE_CADET_ENABLE            = no
+GRAVE_ESC_ENABLE              = no
