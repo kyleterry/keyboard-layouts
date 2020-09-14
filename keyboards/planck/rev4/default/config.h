95b3d93c84649e5cd52077103e8de624a98bf12c
Kyle Terry <kyle@kyleterry.com>
2020-09-14 13:51:29 -0700

    adds planck rev4

---
+++ keyboards/planck/rev4/default/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#if !defined(NO_DEBUG) && !defined(CONSOLE_ENABLE)
+
+#define NO_DEBUG
+
+#endif // !NO_DEBUG
+
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+
+#define NO_PRINT
+
+#endif // !NO_PRINT
+
+#define NO_ACTION_MACRO
+
+#define NO_ACTION_FUNCTION
+
+#define DISABLE_LEADER
+#define NO_MUSIC_MODE
---
+++ keyboards/planck/rev4/default/rules.mk
@@ -0,0 +1,6 @@
+RGBLIGHT_ENABLE               = no
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+OLED_DRIVER_ENABLE            = no
+LEADER_ENABLE                 = no
+SPACE_CADET_ENABLE            = no
+GRAVE_ESC_ENABLE              = no
